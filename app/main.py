6baaf31616afcb14bc46e1f09f985b6960178db4
Anjaneyulu30 <badinenivenkataanjaneyulu@gmail.com>
2026-07-04 14:55:54 +0530

    added database configuration and user model

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 
+from app.database.base import Base
+from app.database.database import engine
+
+# Import all models
+from app.models import User
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="AI Interview Assistant",
     version="1.0.0"
 )
+
 
 @app.get("/")
 def home():
